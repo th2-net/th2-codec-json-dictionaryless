85d37595ea2709afdacc25d5b17dd5c1183bff7a
isengrims <104489572+isengrims@users.noreply.github.com>
2022-06-02 13:57:35 +0300

    [TH2-3708] Initial dictionaryless json codec implementation.
    [TH-3708] Initial dictionaryless json codec implementation.

---
+++ src/main/kotlin/com/exactpro/th2/codec/json/JsonPipelineCodec.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2022-2022 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.exactpro.th2.codec.json
+
+import com.exactpro.th2.codec.api.IPipelineCodec
+import com.exactpro.th2.codec.json.JsonCodecFactory.Companion.PROTOCOL
+import com.exactpro.th2.common.grpc.AnyMessage
+import com.exactpro.th2.common.grpc.AnyMessage.KindCase.RAW_MESSAGE
+import com.exactpro.th2.common.grpc.Direction.FIRST
+import com.exactpro.th2.common.grpc.Direction.SECOND
+import com.exactpro.th2.common.grpc.EventID
+import com.exactpro.th2.common.grpc.MessageGroup
+import com.exactpro.th2.common.grpc.MessageID
+import com.exactpro.th2.common.grpc.RawMessage
+import com.exactpro.th2.common.message.direction
+import com.exactpro.th2.common.message.messageType
+import com.exactpro.th2.common.message.plusAssign
+import com.fasterxml.jackson.databind.InjectableValues
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.ObjectReader
+import com.fasterxml.jackson.databind.ObjectWriter
+import com.fasterxml.jackson.databind.cfg.ContextAttributes
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.google.protobuf.ByteString
+import kotlin.text.Charsets.UTF_8
+
+class JsonPipelineCodec(settings: JsonPipelineCodecSettings): IPipelineCodec {
+    private val objectMapper = ObjectMapper().apply {
+        if(settings.encodeTypeInfo || settings.decodeTypeInfo) {
+            val module = SimpleModule()
+            if(settings.encodeTypeInfo) module.addSerializer(String::class.java, TypeInfoSerializer())
+            if(settings.decodeTypeInfo) {
+                val listType = typeFactory.constructCollectionType(List::class.java, Object::class.java)
+                val mapType = typeFactory.constructMapType(Map::class.java, String::class.java, Object::class.java)
+                module.addDeserializer(Any::class.java, TypeInfoDeserializer(listType, mapType))
+            }
+            registerModule(module)
+        }
+    }
+
+    override fun decode(messageGroup: MessageGroup): MessageGroup {
+        val builder = MessageGroup.newBuilder()
+
+        for (message in messageGroup.messagesList) {
+            if (!message.hasRawMessage()) {
+                builder.addMessages(message)
+                continue
+            }
+
+            val rawMessage = message.rawMessage
+            val metadata = rawMessage.metadata
+
+            if (metadata.run { protocol.isNotBlank() && protocol != PROTOCOL }) {
+                builder.addMessages(message)
+                continue
+            }
+
+            val direction = metadata.id.direction
+
+            require(direction == FIRST || direction == SECOND) { "Unsupported message direction: $direction" }
+
+            val body = rawMessage.body.toByteArray().toString(UTF_8)
+
+            builder += objectMapper.readValue(body, Map::class.java).toProtoBuilder().apply {
+                if (rawMessage.hasParentEventId()) parentEventId = rawMessage.parentEventId
+
+                metadataBuilder.apply {
+                    putAllProperties(metadata.propertiesMap)
+                    messageType = if (direction == FIRST) INCOMING else OUTGOING
+                    protocol = PROTOCOL
+                    id = metadata.id
+                }
+            }
+        }
+
+        return builder.build()
+    }
+
+    override fun encode(messageGroup: MessageGroup): MessageGroup {
+        val builder = MessageGroup.newBuilder()
+
+        for (message in messageGroup.messagesList) {
+            if (!message.hasMessage()) {
+                builder.addMessages(message)
+                continue
+            }
+
+            val parsedMessage = message.message
+            val metadata = parsedMessage.metadata
+
+            if (metadata.run { protocol.isNotBlank() && protocol != PROTOCOL }) {
+                builder.addMessages(message)
+                continue
+            }
+
+            builder += when(val direction = parsedMessage.direction) {
+                FIRST, SECOND -> objectMapper.writeValueAsString(parsedMessage.toMap())
+                else -> error("Unsupported message direction: $direction")
+            }.toByteArray(UTF_8).toRawMessage(
+                metadata.id,
+                metadata.propertiesMap,
+                parsedMessage.parentEventId
+            )
+
+        }
+
+        return builder.build()
+    }
+
+    companion object {
+        private const val INCOMING = "Incoming"
+        private const val OUTGOING = "Outgoing"
+
+        private fun ByteArray.toRawMessage(
+            messageId: MessageID,
+            metadataProperties: Map<String, String>,
+            eventID: EventID
+        ): RawMessage = RawMessage.newBuilder().apply {
+            body = ByteString.copyFrom(this@toRawMessage)
+            parentEventId = eventID
+            metadataBuilder.apply {
+                protocol = PROTOCOL
+                putAllProperties(metadataProperties)
+                id = messageId
+            }
+        }.build()
+    }
+}
---
+++ src/test/kotlin/com/exactpro/th2/codec/json/EncodeTest.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2022-2022 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.exactpro.th2.codec.json
+
+import com.exactpro.th2.codec.json.JsonCodecFactory.Companion.PROTOCOL
+import com.exactpro.th2.common.grpc.AnyMessage
+import com.exactpro.th2.common.grpc.Direction
+import com.exactpro.th2.common.grpc.Message
+import com.exactpro.th2.common.grpc.MessageGroup
+import com.exactpro.th2.common.message.direction
+import com.exactpro.th2.common.message.set
+import com.exactpro.th2.common.value.add
+import com.exactpro.th2.common.value.listValue
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.text.Charsets.UTF_8
+
+
+class EncodeTest {
+    @Test
+    fun testAnyMessageEncodeTest() {
+        val expectedJsonString = """
+            {
+              "numberField": 123,
+              "listOfObjects": [
+                {
+                  "objectField": "value"
+                },
+                {
+                  "numberField": 123
+                }
+              ],
+              "booleanValue": true,
+              "listOfPrimitives": [
+                1.1,
+                2.2
+              ],
+              "stringField": "stringValue",
+              "object": {
+                "objectField": "objectValue"
+              }
+            }
+        """.trimIndent()
+        val eventId = "123"
+
+        val codecSettings = JsonPipelineCodecSettings(true, true)
+        val codec = JsonPipelineCodec(codecSettings)
+
+        val message = Message.newBuilder().apply {
+            direction = Direction.FIRST
+            this["stringField"] = "stringValue"
+            this["numberField"] = "number(123)"
+            this["booleanValue"] = "boolean(true)"
+            this["object"] = Message.newBuilder().apply {
+                this["objectField"] = "objectValue"
+            }
+            this["listOfPrimitives"] = listOf("number(1.1)", "number(2.2)")
+            this["listOfObjects"] = listValue().apply {
+                this.add(Message.newBuilder().apply {
+                    this["objectField"] = "value"
+                })
+                this.add(Message.newBuilder().apply {
+                    this["numberField"] =  "number(123)"
+                })
+            }
+            parentEventIdBuilder.id = eventId
+            metadataBuilder.protocol = PROTOCOL
+        }
+        val messageGroup = MessageGroup.newBuilder().addMessages(AnyMessage.newBuilder().setMessage(message)).build()
+        val result = codec.encode(messageGroup).getMessages(0).rawMessage.body.toString(UTF_8)
+        assertEquals(MAPPER.readTree(result), MAPPER.readTree(expectedJsonString))
+    }
+    companion object {
+        private val MAPPER = ObjectMapper()
+    }
+}
